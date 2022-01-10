5b0345d5f0b1139377f763bded92fb517fdde330
arrahman ali <semangat.programmer@gmail.com>
2022-01-10 19:41:42 +0700

    tambah catatan cmd lengkap

--- kedua.js
+++ kedua.js
@@ -1,9 +1,18 @@
 const ucapan = function (apa){
     return `selamat ${apa}`
-};
+    };
+    
+    console.log(ucapan);
+    let nama = 'kayyisa ali';
+    console.log(`selamat datang {nama}`);
+    
+    
+    
+nama
+let umur=2;
+umur
+let pekerjaan="dokter anak";
+const hobi=(usia,ingin)=>{
 
-console.log(ucapan);
-let nama = 'kayyisa ali';
-console.log(`selamat datang {nama}`);
-
-
+console.log(`mantap {nama}, usia {usia} dan udh punya keingingan untuk {ingin}`)
+}
