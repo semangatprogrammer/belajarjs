057ac74a9e0ef43181cc1b20688ea40101b1ad61
arrahman ali <semangat.programmer@gmail.com>
2022-01-10 19:10:07 +0700

    ok

--- jenisobject.js
+++ jenisobject.js
@@ -1,18 +1,18 @@
 let Mahasiswa = {
-    nama : "kayyisa",
-    usia : "1 tahun 4 bulan",
-    energy : 5,
-    makan : function (porsi){
+    nama: "kayyisa",
+    usia: "1 tahun 4 bulan",
+    energy: 5,
+    makan: function (porsi) {
         this.energy += porsi;
         console.log(`selamat makan sebanyak {porsi} porsi`);
     },
-    minum : function (porsi){
+    minum: function (porsi) {
         this.energy += porsi;
         console.log(`selamat minum sebanyak {porsi} porsi`);
 
     },
-    tidur : function (jam){
-        this.energy = this.energy+(2*jam);
+    tidur: function (jam) {
+        this.energy = this.energy + (2 * jam);
         console.log(`selamat istirahat sebanyak {jam} jam`);
     }
 }
