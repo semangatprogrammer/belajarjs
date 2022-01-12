3d3c545c3b9e16d60b3d2b108e08ab7fb8cfbb21
arrahman <semangat.programmer@gmail.com>
2022-01-12 14:11:15 +0700

    belajar menangkap data react

--- lanjutan/app.js
+++ lanjutan/app.js
@@ -1,3 +1,15 @@
 const fs = require('fs')
-fs.writeFileSync()
-console.log(fs)
+
+//menuliskan file ke syncronous
+//fs.writeFileSync('test.text', 'Hello  World wahai teman teman')
+
+//fs.writeFileSync('data/test.text', 'Hello  World aaaa')
+
+//console.log(fs)
+
+//cara membuat try catch error
+try {
+    fs.writeFileSync('data/test.text', 'Hello  World secara syncronous')
+} catch (e) {
+    console.log(e)
+}
