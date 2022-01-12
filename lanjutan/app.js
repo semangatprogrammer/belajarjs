b5b85e2a4fa046e79f7b353d9e1863b5c3f7f5f8
arrahman <semangat.programmer@gmail.com>
2022-01-12 14:34:51 +0700

    penambahan file write syn dan asyn

--- lanjutan/app.js
+++ lanjutan/app.js
@@ -4,12 +4,19 @@ const fs = require('fs')
 //fs.writeFileSync('test.text', 'Hello  World wahai teman teman')
 
 //fs.writeFileSync('data/test.text', 'Hello  World aaaa')
-
 //console.log(fs)
 
-//cara membuat try catch error
-try {
-    fs.writeFileSync('data/test.text', 'Hello  World secara syncronous')
-} catch (e) {
-    console.log(e)
-}
+fs.writeFile(
+    'data/test.txt',
+    'Hello World menulis data secara asyncronous',
+    (err) => {
+        console.log(err)
+    },
+)
+
+//cara sinkronus
+// try {
+//     fs.writeFileSync('data/test.text', 'Hello  World secara syncronous')
+// } catch (e) {
+//     console.log(e)
+// }
